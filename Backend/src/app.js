f9676220e3b8767c59f20ce880a2ec8d5113c80b
yagamikisuke <fabio.evangelion.guitar@gmail.com>
2020-04-01 15:35:10 -0300

    Validação c/ celebrate e testes com Jest e Supertest

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const cors = require('cors'); //módulo de segurança
+const cors = require('cors'); //Módulo de segurança
+const {errors} = require('celebrate'); //Módulo de validação do expressão que utilizada a biblioteca Joi
 const routes = require('./routes');
 const app = express();
 
 app.use(cors()); //Da forma que está, toda aplicação front-end poderá acessar a aplicação, mas se definida a origin em um objeto passado como parâmetro, apenas essa origgem poderá acessar.
 app.use(express.json()); //Informando o express que as requições que tem corpo são do formato JSON para ele converter para um objeto JS
 app.use(routes);
+app.use(errors())
 
-
-app.listen(3333);
+module.exports = app;
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,10 +1,43 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const ongController = require('./controllers/OngController');
 const incidentController = require('./controllers/IncidentController');
 const profileController = require('./controllers/ProfileController');
 const sessionControler = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions', sessionControler.create);
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), ongController.create);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), profileController.index);
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), incidentController.index);
+
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), incidentController.delete);
 
 /**
  * Rota/Recurso
@@ -33,24 +66,15 @@ const routes = express.Router();
 
 
 // routes.post('/ongs', async (request, response) => {
-    
-    //     /*
-    //     const params = request.query; //Contém os dados da requição do tipo Query Param
-    //     const params = request.params //Contém os dados da requição do tipo Route Param
-    //     const body = request.body //Contém os dados passados em requição que contenha um corpo. Preciso passar para o express a informação que estaremos usando json nas requições para que ele faça o parse para um objeto JS
-    
-    //     console.log(body);
-    //     */
-    
-    // });
-    routes.post('/sessions', sessionControler.create);
-    routes.get('/ongs', ongController.index);
-    routes.post('/ongs', ongController.create);
 
-    routes.get('/profile', profileController.index);
+//     /*
+//     const params = request.query; //Contém os dados da requição do tipo Query Param
+//     const params = request.params //Contém os dados da requição do tipo Route Param
+//     const body = request.body //Contém os dados passados em requição que contenha um corpo. Preciso passar para o express a informação que estaremos usando json nas requições para que ele faça o parse para um objeto JS
 
-    routes.get('/incidents', incidentController.index);
-    routes.post('/incidents', incidentController.create);
-    routes.delete('/incidents/:id', incidentController.delete);
-    
-    module.exports = routes;
+//     console.log(body);
+//     */
+
+// });
+
+module.exports = routes;
